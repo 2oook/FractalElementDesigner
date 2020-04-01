19e40136522d1fd702987e4131e5e2f931b21e21
2oook <iblow82@gmail.com>
2020-04-01 12:33:21 +0400

    Add project files.

---
+++ RC-FE Design - Analysis and synthesis/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using RC_FE_Design___Analysis_and_synthesis.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RC_FE_Design___Analysis_and_synthesis
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var window = new MainWindow();
+            var mvm = new MainViewModel();
+            window.DataContext = mvm;
+            window.Show();
+        }
+    }
+}
---
+++ RC-FE Design - Analysis and synthesis/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RC_FE_Design___Analysis_and_synthesis.ViewModels
+{
+    /// <summary>
+    /// MainViewModel главного окна
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+                
+        }
+
+        #region Глобальные переменные
+
+
+
+        #endregion
+
+        #region Свойства
+
+
+
+        #endregion
+
+        #region Команды
+
+
+
+        #endregion
+
+        #region Методы
+
+
+
+        #endregion
+    }
+}
