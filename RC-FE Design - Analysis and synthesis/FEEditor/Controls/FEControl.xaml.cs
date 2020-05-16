d179f788a65f54c507462ca25386d0b3dfa4f529
2oook <iblow82@gmail.com>
2020-05-16 12:48:26 +0400

    Предварительная реализация canvas

---
+++ RC-FE Design - Analysis and synthesis/FEEditor/Controls/FEControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RC_FE_Design___Analysis_and_synthesis.FEEditor
+{
+    /// <summary>
+    /// Interaction logic for FEControl.xaml
+    /// </summary>
+    public partial class FEControl : UserControl
+    {
+        public FEControl()
+        {
+            InitializeComponent();
+        }
+
+        private void RootBorder_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+
+        }
+
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void UserControl_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void UserControl_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
